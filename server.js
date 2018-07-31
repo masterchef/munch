ce737236ba08de76e242a9c4fadf69c8388ce99b
Viktoras Truchanovicius <vtruchanovicius@linkedin.com>
2018-07-30 21:16:37 -0700

    test

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(credentials, app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
